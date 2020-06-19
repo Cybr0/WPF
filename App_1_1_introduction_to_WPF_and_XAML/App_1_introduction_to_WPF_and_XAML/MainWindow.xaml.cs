43648791e1098176e07594be5306cae71bee6932
Cybr0 <44771809+Cybr0@users.noreply.github.com>
2020-06-20 00:21:02 +0500

    update 3 apps

---
+++ App_1_1_introduction_to_WPF_and_XAML/App_1_introduction_to_WPF_and_XAML/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App_1_introduction_to_WPF_and_XAML
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(text_box_1.Text == string.Empty)
+            {
+                button_1.Content = "Clicked";
+                MessageBox.Show("You don't enter text.");
+            }
+            else
+            {
+                MessageBox.Show("Hi, " + text_box_1.Text);
+                button_1.Content = "Button";
+            }
+        }
+    }
+}
---
+++ App_1_3_crystal_ball/App3/AnswerGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class AnswerGenerator
+    {
+        private static String[] answers = new String[]
+        {
+            "Спроси еще раз.",
+            "Не могу сразу сказать.",
+            "Без сомнений.",
+            "Это действительно так.",
+            "Сосредоточься и спроси снова.",
+            "Нет.",
+            "Да, действительно.",
+            "Я так не считаю.",
+            "Именно так.",
+            "Лучше не говори ничего.",
+            "Плохая точка зрения.",
+            "Вероятно это так.",
+            "Очень сомнительно.",
+            "Как я вижу, то да.",
+            "Мой ответ, нет.",
+            "Конечно.",
+            "Да.",
+            "Можете быть уверены.",
+            "Хорошая точка зрения.",
+            "Смутный ответ, попробуйте снова."
+        };
+
+        public static String GetRandomAnswer()
+        {
+            return answers[new Random().Next(answers.Length)];
+        }
+    }
+}
