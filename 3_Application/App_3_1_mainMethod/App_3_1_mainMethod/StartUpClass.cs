1f3cfe4fc3e2784014f0a218eb7fa94cbfd4070c
Cybr0 <44771809+Cybr0@users.noreply.github.com>
2020-07-14 23:39:30 +0500

    Update reconstruction dirs
    Update reconstruction dirs

---
+++ 3_Application/App_3_1_mainMethod/App_3_1_mainMethod/StartUpClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App_3_1_mainMethod
+{
+    class StartUpClass
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            MainWindow window = new MainWindow();
+            app.Run(window);
+        }
+    }
+}
